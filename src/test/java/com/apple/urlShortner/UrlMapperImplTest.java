a4b8f8b7ba87b1f8f5dfd26086b6a2bd7dcd7756
Mahesh Amruthaluri <amahesh2811@gmail.com>
2022-03-14 15:10:09 -0500

    added test cases

---
+++ src/test/java/com/apple/urlShortner/UrlMapperImplTest.java
@@ -0,0 +1,44 @@
+package com.apple.urlShortner;
+
+import com.apple.urlShortner.model.Url;
+import com.apple.urlShortner.service.UrlMapperImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+
+public class UrlMapperImplTest {
+
+	@Mock
+	UrlMapperImpl _urlMapperImpl;
+
+	@Mock
+	RedisTemplate<String, Url> _redisTemplate;
+
+	@Mock
+	private ValueOperations<String, Url> _valueOperations;
+
+
+	@Before
+	public void setUp() {
+		when(_redisTemplate.opsForValue()).thenReturn(_valueOperations);
+		Mockito.doNothing().when(_valueOperations).set(anyString(), any(Url.class));
+	}
+
+	@Test
+	public void testShortUrl() {
+		_urlMapperImpl = Mockito.spy(new UrlMapperImpl());
+		String url = "www.apple.com";
+		Url shortUrl = _urlMapperImpl.shortUrl(url);
+		assertNotNull(shortUrl);
+	}
+
+}
